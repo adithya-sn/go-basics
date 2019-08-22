d617ec2b97dad52eae35048f3e2570bb0966f344
Adithya <adi.1994@yahoo.in>
2019-08-22 07:17:29 +0530

    mod inc op

--- math.go
+++ math.go
@@ -5,10 +5,8 @@ import "fmt"
 func main() {
 	var num int
 	num = 11
-	fmt.Println(1+2)
-	fmt.Println(4-2)
-	fmt.Println(3*3)
-	fmt.Println(20/5)
-	fmt.Println("num--", )
-	fmt.Println(num++)
+	fmt.Println(1 + 2)
+	fmt.Println(4 - 2)
+	fmt.Println(3 * 3)
+	fmt.Println(20 / 5)
 }
